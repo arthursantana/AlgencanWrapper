e271688f45ea37978b346d2d7d2ab31dec9273f7
Arthur Santana <rockslave@gmail.com>
2020-06-28 18:16:22 -0300

    added dylib

--- lib/convert.sh
+++ lib/convert.sh
@@ -1,4 +1,5 @@
 # Algencan has to be compiled with -fPIC
 # You only need to add it to FFLAGS in the Makefile
 ar -x libalgencan.a; gcc -shared -o ../libalgencan.so *.o -lgfortran
+clang -fpic -shared -Wl, -all_load libalgencan.a -o ../libalgencan.dylib
 rm *.o
